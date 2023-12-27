392b427d64f95e34e20f45ac0e23d9190122f6ce
William Crupi <williamcrupi@Williams-Air.tampabay.rr.com>
2023-12-27 17:46:47 -0500

    Exercise

---
+++ grid.hs
@@ -0,0 +1,3 @@
+grid :: Int -> Int -> [(Int,Int)]
+grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
+square n = [(x,y) | (x,y) <- grid n n, x /= y]
---
+++ squared.hs
@@ -0,0 +1,2 @@
+sumOfSquares :: Int
+sumOfSquares = sum [x^2 | x <- [1..100]]
