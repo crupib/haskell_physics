dd74f99573af622d2136e7e787b0b3301139f2aa
William Crupi <crupib@gmail.com>
2024-01-22 19:34:44 -0500

    votes

--- voting.hs
+++ voting.hs
@@ -1,4 +1,10 @@
 import Data.List
+ballots :: [[String]]
+ballots = [["Red","Green"],
+           ["Blue"],
+           ["Green","Red","Blue"],
+           ["Blue","Green","Red"],
+           ["Green"]]
 votes :: [String]
 votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
 count :: Eq a => a -> [a] -> Int
@@ -10,3 +16,13 @@ result :: Ord a => [a] -> [(Int,a)]
 result vs = sort [(count v vs, v) | v <- rmdups vs]
 winner :: Ord a => [a] -> a
 winner = snd . last . result
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (/= [])
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/= x))
+rank :: Ord a => [[a]] -> [a]
+rank = map snd . result . map head
+winner' :: Ord a => [[a]] -> a
+winner' bs = case rank (rmempty bs) of
+               [c] -> c
+               (c:cs) -> winner' (elim c bs)
