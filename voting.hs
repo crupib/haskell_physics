3a580fd53039e030ad915f773119737dbaa6d364
William Crupi <crupib@gmail.com>
2024-01-22 19:17:25 -0500

    votes, base

--- voting.hs
+++ voting.hs
@@ -1,3 +1,4 @@
+import Data.List
 votes :: [String]
 votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
 count :: Eq a => a -> [a] -> Int
@@ -5,3 +6,7 @@ count x = length . filter (== x)
 rmdups :: Eq a => [a] -> [a]
 rmdups [] = []
 rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+result :: Ord a => [a] -> [(Int,a)]
+result vs = sort [(count v vs, v) | v <- rmdups vs]
+winner :: Ord a => [a] -> a
+winner = snd . last . result
