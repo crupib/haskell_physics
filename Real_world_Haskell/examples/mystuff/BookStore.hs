19a79d38112275ac92f48bdb7c8f77dffe7fb44d
crupib <crupib@gmail.com>
2024-02-05 12:53:23 -0500

    Real Haskell

--- Real_world_Haskell/examples/mystuff/BookStore.hs
+++ Real_world_Haskell/examples/mystuff/BookStore.hs
@@ -1,9 +1,23 @@
 type CustomerID = Int
 type ReviewBody = String
+type BookRecord = (BookInfo, BookReview)
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+data BookReview = BookReview BookInfo CustomerID String
+data Bool = False | True
 data BetterReview = BetterReview BookInfo CustomerID ReviewBody
 data BookInfo = Book Int String [String]
                 deriving (Show)
 data MagazineInfo = Magazine Int String [String]
                     deriving (Show)
-data BookReview = BookReview BookInfo CustomerID String
+data Customer = Customer {
+      customerID      :: CustomerID
+    , customerName    :: String
+    , customerAddress :: Address
+    } deriving (Show)
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                 | CashOnDelivery
+                 | Invoice CustomerID
+                   deriving (Show)
 myInfo = Book 9780135072455 "Algebra of Programming" ["Richard Bird", "Oege de Moor"]
